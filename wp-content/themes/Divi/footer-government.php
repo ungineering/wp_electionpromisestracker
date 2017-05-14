eea14268e1a345f0e871a2fe0a18029858b2ca43
shubham <shubham.sarin@paytm.com>
2017-05-14 23:58:28 +0530

    added common Divi header for manifesto page, and removed previous

--- wp-content/themes/Divi/footer-government.php
+++ wp-content/themes/Divi/footer-government.php
@@ -1,39 +1,4 @@
-<nav class="navbar navbar-default navbar-fixed-top">
 
-            <div class="container">
-
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <a class="navbar-brand" href="/">
-                        <b>Election Promises Tracker</b>
-                    </a>
-
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <form class="navbar-form navbar-right" action="mailto:joinus@electionpromisestracker.in?Subject=I want to contribute" method="get"-->
-                        <button type="submit" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" title="Want to Join us?">Join Us</button>
-                    </form>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="/about">About Us</a></li>
-                    </ul>
-                </div>
-                <!-- /.navbar-collapse -->
-
-            </div>
-            <!-- /.container-fluid -->
-
-        </nav>
-        
 
         <div class="container-fluid">
             <div class="page-header">
